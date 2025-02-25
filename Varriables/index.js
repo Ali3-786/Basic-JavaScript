1bb24ca8a2a6c6863ba02040ed3ca88d2b6a5af0
Ali3-786 <alibutt201992732@gmail.com>
2025-02-25 22:24:16 +0500

    Varriables Pratice And Rules

---
+++ Varriables/index.js
@@ -0,0 +1,20 @@
+var first_Para = "This is first Paragraph";
+console.log(first_Para);
+
+var first_Para$ = "This is first Paragraph";
+console.log(first_Para$);
+
+// var first@Name = "This is first Paragraph";
+// console.log(first@Name);
+
+var first$name = "This is JavaScript";
+console.log(first$name);
+
+var firstName = "This is Pararap";
+console.log(firstName);
+
+// var first Class = "Java Script Class";
+// console.log(first Class);
+
+var myFirstVarriableNameJavaScript = "This is Long Varriable Name";
+console.log(myFirstVarriableNameJavaScript);
